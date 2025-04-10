86b4c6cc84a031563d158e9b37de5fd0bb8448a4
Sandeepgoud Yelikatte <129197139+sandeepgoud1215@users.noreply.github.com>
2025-04-10 19:48:49 +0530

    Delete src/utils.py

--- src/utils.py
+++
@@ -1,9 +0,0 @@
-# src/utils.py
-
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-def split_features_labels(df, target_column="y"):
-    X = df.drop(target_column, axis=1)
-    y = df[target_column]
-    return train_test_split(X, y, test_size=0.2, random_state=42)
