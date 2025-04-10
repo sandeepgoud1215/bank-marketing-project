30d378df27ffa87c83e7797a0baddc24de5b21cf
Sandeepgoud Yelikatte <129197139+sandeepgoud1215@users.noreply.github.com>
2025-04-10 20:59:52 +0530

    Create training.py

---
+++ src/training.py
@@ -0,0 +1,11 @@
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+def train_model(X, y):
+    model = RandomForestClassifier()
+    model.fit(X, y)
+    return model
+
+def save_model(model, filename='model.pkl'):
+    with open(filename, 'wb') as f:
+        pickle.dump(model, f)
